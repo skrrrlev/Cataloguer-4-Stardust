fdd361169ff45113eb40e1ad04abf0e1065f2951
skrrrlev <reimer.frickmann@gmail.com>
2022-10-12 12:54:57 +0200

    Include the eazy translation file in the pip package.

--- setup.py
+++ setup.py
@@ -6,13 +6,18 @@ setup(
     author = "Ditlev Frickmann @skrrrlev",
     author_email = "reimer.frickmann@gmail.com",
     description = "A tool for easily creating the necessary input files for the Stardust SED fitting code using your own data.",
-    version = "0.0.4",
+    version = "0.0.5",
     license = "MIT",
     url = "https://github.com/skrrrlev/Cataloguer-4-Stardust.git",  
     packages=[
         'C4S',
         'C4S.dataclasses',
+        'C4S.eazy',
     ],
+    package_data={
+        'C4S.eazy': ['*.txt']
+    },
+    include_package_data=True,
     install_requires = [
         'numpy',
         'astropy',
